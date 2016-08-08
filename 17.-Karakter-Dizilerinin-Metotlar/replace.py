ac93812cf70baae3a37e72d7b38996c900dc91b2
Özgür <ozgurturkiye@gmail.com>
2016-08-08 23:38:45 +0300

    Create replace.py

---
+++ 17.-Karakter-Dizilerinin-Metotlar/replace.py
@@ -0,0 +1,9 @@
+##replace() metodunu kullanarak kardiz karakter dizisinin içinde geçen “e” harfini “E” ile değiştirelim.
+
+kardiz = "elma"
+print (kardiz.replace("e", "E"))
+
+## 2- Birden fazla karakter değiştirebilir.
+
+sehir = "istanbul"
+print (sehir.replace("ist","İST"))
