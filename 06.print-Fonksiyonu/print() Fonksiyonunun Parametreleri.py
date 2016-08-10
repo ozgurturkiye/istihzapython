ca79cade487eb33bb2334bd3d328411d494a01c0
Özgür <ozgurturkiye@gmail.com>
2016-08-11 02:39:02 +0300

    ASCII Karakter seti listeleme

--- 06.print-Fonksiyonu/print() Fonksiyonunun Parametreleri.py
+++ 06.print-Fonksiyonu/print() Fonksiyonunun Parametreleri.py
@@ -10,3 +10,11 @@ print("Tahir olmak da ayıp değil", "Zühre olmak da", sep=" ", end="\n", file=
 print(*"Linux", sep=".")    # output: L.i.n.u.x
 print(*"Beşiktaş")          # output: B e ş i k t a ş
 print(*"Beşiktaş", sep="-") # output: B-e-ş-i-k-t-a-ş
+
+#ASCII karakter tablosunu yazdıran program örneği
+
+for i in range(128):
+    if i % 4 == 0:
+        print("\n")
+
+    print("{:<3}{:>8}\t".format(i, repr(chr(i))), sep="", end="")
