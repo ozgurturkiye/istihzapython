0a7a0a2c18b4984957e79a947d80640ee0b05d56
hurol <hurolyalcin@gmail.com>
2016-08-10 19:00:29 +0300

    Add files via upload

---
+++ 17.-Karakter-Dizilerinin-Metotlari/5_endswith-startswith.py
@@ -0,0 +1,40 @@
+## endswith(), startswith()
+
+
+## endswith() - ornek_1
+kardiz = "istihza"
+print(kardiz.endswith("a"))
+
+
+#### endswith() - ornek_2
+##d1 = "python.ogg"
+##d2 = "tkinter.mp3"
+##d3 = "pygtk.ogg"
+##d4 = "movie.avi"
+##d5 = "sarki.mp3"
+##d6 = "filanca.ogg"
+##d7 = "falanca.mp3"
+##d8 = "dosya.avi"
+##d9 = "perl.ogg"
+##d10 = "c.avi"
+##d11 = "c++.mp3"
+##for i in d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11:
+##    if i.endswith(".mp3"):
+##        print(i)
+##
+##
+#### startswith() - ornek_1
+##d1 = "python.ogg"
+##d2 = "tkinter.mp3"
+##d3 = "pygtk.ogg"
+##d4 = "movie.avi"
+##d5 = "sarki.mp3"
+##d6 = "filanca.ogg"
+##d7 = "falanca.mp3"
+##d8 = "dosya.avi"
+##d9 = "perl.ogg"
+##d10 = "c.avi"
+##d11 = "c++.mp3"
+##for i in d1, d2, d3, d4, d5, d6, d7, d8, d9, d10, d11:
+##    if i.startswith("p"):
+##        print(i)
