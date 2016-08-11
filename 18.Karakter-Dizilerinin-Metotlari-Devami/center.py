8b8ed46061caabd6a5bf62c5b65a1f06d806a0b9
hurol <hurolyalcin@gmail.com>
2016-08-11 18:46:15 +0300

    Create center.py

---
+++ 18.Karakter-Dizilerinin-Metotlari-Devami/center.py
@@ -0,0 +1,11 @@
+## center()
+
+
+#### center() - ornek_1
+##for metot in dir(""):       ## dir() - metotları listele
+##    print(metot.center(15))
+
+
+## center() - ornek_2
+kardiz = 'python'
+print(kardiz.center(10,'-'))
