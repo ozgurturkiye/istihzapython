351229c82b123e58cde4cbc1dad9948b118fe137
Özgür <ozgurturkiye@gmail.com>
2016-08-17 03:07:49 +0300

    Create Liste uretecleri (List Comprehensions).py

---
+++ 21.Listeler-ve-Demetler/Liste uretecleri (List Comprehensions).py
@@ -0,0 +1,18 @@
+#Liste üreteçleri - (List Comprehensions)
+#Aşağıdaki 0-100 arası liste oluşturmak için 3 yöntem
+liste = [i for i in range(100)]
+
+liste1 = []
+
+for i in range(100):
+    liste1 += [i]
+
+liste2 = list(range(100))
+
+print(liste)
+print(liste1)
+print(liste2)
+
+#0-100 arası çift sayılardan oluşan liste oluşturmak
+liste = [i for i in range(100) if i % 2 == 0]
+print(liste)
