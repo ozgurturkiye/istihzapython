2bf9542f43c9e25595f6a9e81cf23cc7ce62d402
Özgür <ozgurturkiye@gmail.com>
2016-08-18 05:25:07 +0300

    Update and rename Liste uretecleri (List Comprehensions).py to 03_Liste uretecleri (List Comprehensions).py

--- 21.Listeler-ve-Demetler/03_Liste uretecleri (List Comprehensions).py
+++ 21.Listeler-ve-Demetler/03_Liste uretecleri (List Comprehensions).py
@@ -16,3 +16,5 @@ print(liste2)
 #0-100 arası çift sayılardan oluşan liste oluşturmak
 liste = [i for i in range(100) if i % 2 == 0]
 print(liste)
+
+#Buraya da 0-100 arası 10 ar 10 ar artan liste oluşturalım. Tabi üreteçler ile
