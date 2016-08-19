9405b897c75668988b2b524af08fe24070a7f19e
Özgür <ozgurturkiye@gmail.com>
2016-08-19 16:59:04 +0300

    Uygulamsını yapmanın faydalı olacağı 4 program

---
+++ 000.JustForFunPrograms/007_dort_uygulama_programi.py
@@ -0,0 +1,19 @@
+#Bu kısımda 4 uygulama programı yazalım bakalım Python'da ne kadar ilerlemişiz.
+#1. Program: 4 işlem yapan hesap makinası
+#2. Program: Kullanıcı ad, soyad, doğum tarihi, cinsiyet bilgilerini alıp dosyaya kaydeden program
+#3. Program: Kullanıcı adı ve parola ile doğru giriş yapıldığında(dosyadan kontrol edilecek) 
+            ##dosyadan kayıtlı kullanıcı bilgilerini çeken ekrana yazdıran program
+#4. Program: Girilen ismin binary karşılığını dosyaya yazan ve 
+            ##dosyadaki binary veriyi karakter dizisi(unicode - utf8) olarak okuyan program
+
+
+#1. Program - Hesap makinası Let's begin :)
+metin == """
+(1) Toplama
+(2) Çıkarma
+(3) Çarpma
+(4) Bölme
+Seçiminizi yapınız
+Çıkmak için (q) ya basınız
+"""
+#devam edecek :)
