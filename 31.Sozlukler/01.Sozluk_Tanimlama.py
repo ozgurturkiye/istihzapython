2b0bd661ce9186912eb97ba2da54e952924ee035
hurturkiye <hurolyalcin@gmail.com>
2016-08-27 23:20:05 +0300

    Create 01.Sozluk_Tanimlama.py

---
+++ 31.Sozlukler/01.Sozluk_Tanimlama.py
@@ -0,0 +1,18 @@
+## Sozlukler - Tanımlama
+
+
+## Tanımlama - {}
+
+bos_sozluk = {}
+kelimeler = {"kitap": "book"}
+print(bos_sozluk)               ## output: {}
+print(type(bos_sozluk))         ## output: <class 'dict'>
+print(kelimeler)                ## output: {'kitap': 'book'}
+print(type(kelimeler))          ## output: <class 'dict'>
+print(len(kelimeler))
+sozluk = {"kitap": "book",
+          "bilgisayar": "computer",
+          "programlama": "programming",
+          "dil": "language",
+          "defter": "notebook"}
+print(sozluk)                   
