ef47eca7e7e6ae034e1f7623a3cf29c1abf9788a
hurturkiye <hurolyalcin@gmail.com>
2016-08-31 01:01:41 +0300

    Create 11.update_metodu.py

---
+++ 32.Sozluklerin_Metotlari/11.update_metodu.py
@@ -0,0 +1,11 @@
+## Sözlüklerin Metotları
+
+## update() metodu
+
+stok      = {"elma": 5, "armut": 10, "peynir": 6, "sosis": 15}
+
+yeni_stok = {"elma": 3, "armut": 12, "peynir": 2, "sosis": 25}
+
+stok.update(yeni_stok)
+
+print(stok)     ## output: {'elma': 3, 'armut': 12, 'peynir': 2, 'sosis': 25}
