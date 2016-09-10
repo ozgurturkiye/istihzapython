8e6cf3d3cb1688af659438de808a6f4363615737
hurturkiye <hurolyalcin@gmail.com>
2016-09-10 23:39:00 +0300

    Create 01.modulleri_iceri_aktarmak_2.py

---
+++ 37.Moduller/01.modulleri_iceri_aktarmak_2.py
@@ -0,0 +1,13 @@
+## Modüller
+
+## 02_Modülleri İçeri Aktarmak
+
+##import subprocess
+##
+##subprocess.call('notepad.exe')
+
+import subprocess as sp
+import webbrowser as web
+
+sp.call('notepad.exe')
+web.open('www.google.com')
