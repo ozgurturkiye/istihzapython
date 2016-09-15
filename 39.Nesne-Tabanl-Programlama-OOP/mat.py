d30f993c525366ade4519e1a650bf5a43aec78c5
hurturkiye <hurolyalcin@gmail.com>
2016-09-15 22:57:38 +0300

    Statik Metotlar - Matematik modülü
    Matematik işlemler yapmak için hazırlanmış Math sınıfını ve statik metotları barındır.

---
+++ 39.Nesne-Tabanl-Programlama-OOP/mat.py
@@ -0,0 +1,13 @@
+## Matematik modülü
+
+class Math():
+    '''Matematik işlemleri yapmamızı sağlayan
+    bir sınıf.'''
+
+    @staticmethod
+    def pi():
+        return 22/7
+
+    @staticmethod
+    def karekok(sayı):
+        return sayı ** 0.5
